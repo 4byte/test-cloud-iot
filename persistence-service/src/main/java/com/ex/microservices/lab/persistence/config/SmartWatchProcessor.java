aaefb12a16822200bbaf344b9cc55c578b0ee8ea
4byte <medhigh.medhigh@gmail.com>
2018-12-21 00:21:59 +0200

    fix: working Processor

--- persistence-service/src/main/java/com/ex/microservices/lab/persistence/config/SmartWatchProcessor.java
+++ persistence-service/src/main/java/com/ex/microservices/lab/persistence/config/SmartWatchProcessor.java
@@ -1,19 +1,19 @@
 package com.ex.microservices.lab.persistence.config;
 
-import org.springframework.cloud.stream.annotation.*;
-import org.springframework.messaging.MessageChannel;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.messaging.SubscribableChannel;
 
 @EnableBinding
-public interface SmartWatchProcessor {
-		String INPUT = "smart_watch";
+public interface SmartWatchProcessor extends Processor {
+	String INPUT = "input";
+	String WORDS = "words";
 
-		@Input
-		SubscribableChannel myInput();
+	@Input(SmartWatchProcessor.WORDS)
+	SubscribableChannel input();
 
-		@Output("myOutput")
-		MessageChannel anOutput();
-
-		@Output
-		MessageChannel anotherOutput();
+	@Input(SmartWatchProcessor.INPUT)
+	SubscribableChannel myInput();
 }
